9a3714d4d06c6e1fd3992d0436753ad4d253b8f6
Aleksandr Menshchikov <aleksandr.menshchikov1982@gmail.com>
2020-05-06 15:39:24 +0300

    fix: fix bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 const closeIcon = document.querySelector(".pop-up__close-icon");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const popUp = document.querySelector(".pop-up");
-const formInputList = document.querySelectorAll(".form__input");
+const formInputName = document.querySelector(".form__input_name");
+const formInputProfession = document.querySelector(".form__input_profession");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 const form = document.querySelector(".form");
@@ -18,14 +19,14 @@ const closePopupByEsc = (e) => {
 
 const editProfile = () => {
   popUp.classList.add("pop-up_opened");
-  formInputList[0].value = profileTitle.textContent;
-  formInputList[1].value = profileSubtitle.textContent;
+  formInputName.value = profileTitle.textContent;
+  formInputProfession.value = profileSubtitle.textContent;
 };
 
 const submitForm = (e) => {
   e.preventDefault();
-  profileTitle.textContent = formInputList[0].value;
-  profileSubtitle.textContent = formInputList[1].value;
+  profileTitle.textContent = formInputName.value;
+  profileSubtitle.textContent = formInputProfession.value;
   closePopup();
 };
 
