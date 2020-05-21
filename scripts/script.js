eb46e0418e3f12a8981805dbe7acdb015d155bcf
Aleksandr Menshchikov <aleksandr.menshchikov1982@gmail.com>
2020-05-21 18:14:17 +0300

    refactor: improve script.js

--- scripts/script.js
+++ scripts/script.js
@@ -30,8 +30,6 @@ const initialCards = [
   },
 ];
 
-const closeIcon = document.querySelector(".pop-up__close-icon");
-const profileEditButton = document.querySelector(".profile__edit-button");
 const popUp = document.querySelector(".pop-up");
 const formInputName = document.querySelector(".form__input_name");
 const formInputProfession = document.querySelector(".form__input_profession");
@@ -39,71 +37,75 @@ const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 const formProfile = document.querySelector(".form_profile");
 const elementsList = document.querySelector(".elements__list");
-const formInputNameCard = document.querySelector(".form__input_name-card");
-const formInputLinkCard = document.querySelector(".form__input_link-card");
-const profileAddButton = document.querySelector(".profile__add-button");
 const formCard = document.querySelector(".form_card");
 const popUpImage = document.querySelector(".pop-up-image");
-const popUpImageCloseIcon = document.querySelector(".pop-up-image__close-icon");
 
-const closePopup = () => {
-  popUp.classList.remove("pop-up_opened");
-  popUp.classList.add("pop-up_closed");
-  setTimeout(() => {
-    popUp.classList.remove("pop-up_closed");
-  }, 200);
+const openAndClosePopup = (e) => {
+  if (e.target.classList.contains("profile__edit-button")) {
+    formCard.classList.add("form_non-active");
+    formProfile.classList.remove("form_non-active");
+    formInputName.value = profileTitle.textContent;
+    formInputProfession.value = profileSubtitle.textContent;
+    popUp.classList.add("pop-up_opened");
+  } else if (e.target.classList.contains("profile__add-button")) {
+    formCard.classList.remove("form_non-active");
+    formProfile.classList.add("form_non-active");
+    popUp.classList.add("pop-up_opened");
+  } else if (e.target.classList.contains("elements__image")) {
+    popUpImage.classList.add("pop-up-image_opened");
+  } else if (e.target.classList.contains("pop-up__close-icon")) {
+    popUp.classList.remove("pop-up_opened");
+  } else if (e.target.classList.contains("pop-up-image__close-icon")) {
+    popUpImage.classList.remove("pop-up-image_opened");
+  }
 };
 
 const closePopupByEsc = (e) => {
   if (e.keyCode === 27 && popUp.classList.contains("pop-up_opened")) {
-    closePopup();
+    popUp.classList.remove("pop-up_opened");
   } else if (
     e.keyCode === 27 &&
     popUpImage.classList.contains("pop-up-image_opened")
   ) {
-    closePopupImage();
+    popUpImage.classList.remove("pop-up-image_opened");
   }
-};
-
-const editProfile = () => {
-  formCard.classList.add("form_non-active");
-  formProfile.classList.remove("form_non-active");
-  popUp.classList.add("pop-up_opened");
-  formInputName.value = profileTitle.textContent;
-  formInputProfession.value = profileSubtitle.textContent;
 };
 
 const submitFormProfile = (e) => {
   e.preventDefault();
   profileTitle.textContent = formInputName.value;
   profileSubtitle.textContent = formInputProfession.value;
-  closePopup();
-};
-
-const addCard = () => {
-  popUp.classList.add("pop-up_opened");
-  formCard.classList.remove("form_non-active");
-  formProfile.classList.add("form_non-active");
+  popUp.classList.remove("pop-up_opened");
 };
 
 const submitFormCard = (e) => {
   e.preventDefault();
+  const formInputNameCard = document.querySelector(".form__input_name-card");
+  const formInputLinkCard = document.querySelector(".form__input_link-card");
   elementsList.prepend(
     createCard(formInputNameCard.value, formInputLinkCard.value)
   );
   formInputNameCard.value = "";
   formInputLinkCard.value = "";
-  closePopup();
+  popUp.classList.remove("pop-up_opened");
 };
 
-const toggleLikeAndRemoveCardAndZoomImage = (e) => {
+const toggleLike = (e) => {
   if (e.target.classList.contains("elements__like")) {
     e.target.classList.toggle("elements__like_active");
-  } else if (e.target.classList.contains("elements__remove")) {
+  }
+};
+
+const removeCard = (e) => {
+  if (e.target.classList.contains("elements__remove")) {
     e.target.closest(".elements__item").remove();
-  } else if (e.target.classList.contains("elements__image")) {
+  }
+};
+
+const zoomImage = (e) => {
+  if (e.target.classList.contains("elements__image")) {
     const popUpImageImg = document.querySelector(".pop-up-image__img");
-    popUpImageImg.src = e.target.src;
+    popUpImageImg.src = e.target.style.backgroundImage.slice(5, -2);
     const popUpImageDescription = document.querySelector(
       ".pop-up-image__description"
     );
@@ -113,18 +115,10 @@ const toggleLikeAndRemoveCardAndZoomImage = (e) => {
   }
 };
 
-const closePopupImage = () => {
-  popUpImage.classList.remove("pop-up-image_opened");
-  popUpImage.classList.add("pop-up-image_closed");
-  setTimeout(() => {
-    popUpImage.classList.remove("pop-up-image_closed");
-  }, 200);
-};
-
 const createCard = (name, link) => {
   const card = document.querySelector("#card").content.cloneNode(true);
   const elementsImage = card.querySelector(".elements__image");
-  elementsImage.src = link;
+  elementsImage.style.backgroundImage = `url(${link})`;
   const elementsItemTitle = card.querySelector(".elements__item-title");
   elementsItemTitle.textContent = name;
   return card;
@@ -134,18 +128,16 @@ initialCards.forEach(({ name, link }) => {
   elementsList.append(createCard(name, link));
 });
 
-profileEditButton.addEventListener("click", editProfile);
+document.addEventListener("click", openAndClosePopup);
 
-closeIcon.addEventListener("click", closePopup);
+document.addEventListener("keyup", closePopupByEsc);
 
 formProfile.addEventListener("submit", submitFormProfile);
 
-profileAddButton.addEventListener("click", addCard);
-
 formCard.addEventListener("submit", submitFormCard);
 
-elementsList.addEventListener("click", toggleLikeAndRemoveCardAndZoomImage);
+elementsList.addEventListener("click", toggleLike);
 
-popUpImageCloseIcon.addEventListener("click", closePopupImage);
+elementsList.addEventListener("click", removeCard);
 
-document.addEventListener("keyup", closePopupByEsc);
+elementsList.addEventListener("click", zoomImage);
